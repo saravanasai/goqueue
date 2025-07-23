805ac94bf743f4fbebf24be9ea881c6f2fda8750
saravanasai <saravanasai002@gmail.com>
2025-07-23 09:51:48 +0530

    Added uuid for JOB id generation

--- adapter/memory/utils.go
+++ adapter/memory/utils.go
@@ -1,12 +1,9 @@
 package memory
 
 import (
-	"strconv"
-	"sync/atomic"
+	"github.com/google/uuid"
 )
 
-var idCounter uint64
-
 func generateID() string {
-	return strconv.FormatUint(atomic.AddUint64(&idCounter, 1), 10)
+	return uuid.New().String()
 }
